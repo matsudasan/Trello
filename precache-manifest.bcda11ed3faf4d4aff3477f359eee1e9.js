dbf7b1045d42024a9c514b39e8e275fa260264e4
matsuda <matsuda12357@gmail.com>
2020-10-12 18:13:38 +0900

    Updates

---
+++ precache-manifest.bcda11ed3faf4d4aff3477f359eee1e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c859e93624ff1400da3f9c5ca4ca2e83",
+    "url": "/Trello/index.html"
+  },
+  {
+    "revision": "6597f1ad7d5901e8b563",
+    "url": "/Trello/static/css/main.aae1fb61.chunk.css"
+  },
+  {
+    "revision": "9a199b68748621965e94",
+    "url": "/Trello/static/js/2.962b7e72.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Trello/static/js/2.962b7e72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6597f1ad7d5901e8b563",
+    "url": "/Trello/static/js/main.1d18cc1d.chunk.js"
+  },
+  {
+    "revision": "d59e13c74e909b98624d",
+    "url": "/Trello/static/js/runtime-main.79405e46.js"
+  }
+]);
